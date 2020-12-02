b0f5b18ade5fcb15c8485e1685b044c966cd541d
JeromeLemay <jeromelemay@hotmail.com>
2020-12-01 19:48:19 -0500

    Ajout message accueil

--- accueil.php
+++ accueil.php
@@ -1,3 +1,12 @@
 <?php include "inc/header.php" ?> 
  
+<h1>Bienvenu chez ProJect!</h1>
+<br>
+<p>Chez ProJect, nous souhaitons vous simplifier la vie en améliorant la manière dont vous travaillez. 
+Nous pouvons réussir cet exploit en ramenant toutes vos tâches à un même endroit. 
+Facile à naviguer et clair d'affichage, tous vos futurs projets seront grandement réussi grace à ProJect!
+</p>
+
+
+
 <?php include "inc/footer.php" ?>
